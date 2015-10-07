b2a5ec739110247ba1ab3bded6c605081811f27f
Alexei Colin <ac@alexeicolin.com>
2015-10-07 17:09:14 -0400

    apps: chain: support MSP-TS430 board

--- apps/chain/src/main.c
+++ apps/chain/src/main.c
@@ -60,13 +60,23 @@ void init()
     PJDIR = BIT(PIN_LED2);
     P2DIR = 0x00;
     P3DIR = BIT(PIN_AUX_1) | BIT(PIN_AUX_2);
-    P4DIR = BIT(PIN_LED1);
+    P4DIR = 0x0;
+
+    GPIO(PORT_LED1, DIR) |= BIT(PIN_LED1);
+    GPIO(PORT_LED2, DIR) |= BIT(PIN_LED2);
+#if defined(PORT_LED3)
+    GPIO(PORT_LED3, DIR) |= BIT(PIN_LED3);
+#endif
 
     // set clock speed to 4 MHz
     CSCTL0_H = 0xA5;
     CSCTL1 = DCOFSEL0 | DCOFSEL1;
     CSCTL2 = SELA_0 | SELS_3 | SELM_3;
     CSCTL3 = DIVA_0 | DIVS_0 | DIVM_0;
+
+#if defined(PORT_LED3) // when available, this LED indicates power-on
+    GPIO(PORT_LED3, OUT) |= BIT(PIN_LED3);
+#endif
 }
 
 static void blink_led1(unsigned blinks, unsigned duty_cycle) {
--- apps/chain/src/pin_assign.h
+++ apps/chain/src/pin_assign.h
@@ -5,23 +5,46 @@
 // (a control bit for TAxCCTLx uses the name 'OUT')
 #undef OUT
 
+// #define BOARD_WISP
+#define BOARD_MSP_TS430
+
 #define BIT_INNER(idx) BIT ## idx
 #define BIT(idx) BIT_INNER(idx)
 
 #define GPIO_INNER(port, reg) P ## port ## reg
 #define GPIO(port, reg) GPIO_INNER(port, reg)
 
+#if defined(BOARD_WISP)
 #define     PORT_LED1           4
-#define 	PIN_LED1			0
+#define     PIN_LED1            0
 #define     PORT_LED2           J
-#define 	PIN_LED2			6
+#define     PIN_LED2            6
 
 #define     PORT_AUX            3
-#define		PIN_AUX_1			4
-#define		PIN_AUX_2			5
+#define        PIN_AUX_1            4
+#define        PIN_AUX_2            5
 
 
-#define		PORT_AUX3			1
-#define		PIN_AUX_3			4
+#define        PORT_AUX3            1
+#define        PIN_AUX_3            4
+
+#elif defined(BOARD_MSP_TS430)
+
+#define     PORT_LED1           1
+#define     PIN_LED1            1
+#define     PORT_LED2           1
+#define     PIN_LED2            2
+#define     PORT_LED3           1
+#define     PIN_LED3            0
+
+#define     PORT_AUX            3
+#define     PIN_AUX_1           4
+#define     PIN_AUX_2           5
+
+
+#define     PORT_AUX3           1
+#define     PIN_AUX_3           4
+
+#endif // BOARD_*
 
 #endif
